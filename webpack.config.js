d8e3af53933d60f14025dc503dbd7fe2c60037ae
prostoleo <73364400+prostoleo@users.noreply.github.com>
2021-06-29 18:15:06 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,233 @@
+//=========================================
+//todo подключаем модуль path из node.js
+const { dirname } = require('path');
+const path = require('path');
+//=========================================
+//todo подключаем плагин cleanwebpack - очищает каждый раз паgre dist
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+//=========================================
+//todo подключаем плагин miniCssExtractPlugin -  создает отдельный файл css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+//=========================================
+//todo подключаем плагин html-webpack-plugin - добавляет html файл в dist папку а также подключает к нему нужные скрипты
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//=========================================
+//todo для минификации css файла
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+//=========================================
+//todo для минификации js файла
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+//=========================================
+//todo анализ финального bundle - можно подключить потом через функцию - все начальные плагины в функцию, а если флаг isProd - то подключаем еще его
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+//=========================================
+//* переменная isDev чтобы определять - находимся ли мы в режиме разработки или build
+const isDev = process.env.NODE_ENV === 'development';
+console.log('isDev: ', isDev);
+const isProd = !isDev;
+
+//=========================================
+//todo функция для оптимизации и минификации кода при build
+function optimize() {
+	const config = {
+		splitChunks: {
+			chunks: 'all',
+		},
+	};
+
+	if (isProd) {
+		config.minimizer = [
+			new OptimizeCssAssetsWebpackPlugin(),
+			new TerserWebpackPlugin(),
+		];
+	}
+
+	return config;
+}
+//=========================================
+//todo функция для вывода названия файлов
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+//=========================================
+
+//todo функция для css loaders
+const cssLoaders = (extra) => {
+	const loaders = [
+		{
+			loader: MiniCssExtractPlugin.loader,
+			options: {},
+		},
+		'css-loader',
+	];
+
+	if (extra) {
+		loaders.push(extra);
+	}
+
+	return loaders;
+};
+
+//=========================================
+
+//todo функция для css loaders
+const babelOptions = (presetsExtra, pluginsExtra) => {
+	const options = {
+		presets: ['@babel/preset-env'],
+		plugins: [],
+	};
+
+	if (presetsExtra) {
+		options.presets = [options.presets, ...presetsExtra];
+	}
+
+	if (pluginsExtra) {
+		options.plugins = [options.plugins, ...pluginsExtra];
+	}
+
+	return options;
+};
+
+//todo для добавления es-lint в режиме разработки( только для js файлов )
+const jsLoaders = () => {
+	const loaders = [
+		{
+			loader: 'babel-loader',
+			options: babelOptions(),
+		},
+	];
+
+	if (isDev) {
+		loaders.push('eslint-loader');
+	}
+
+	return loaders;
+};
+
+//=========================================
+//=========================================
+
+//todo экспортируем объект конфигураций
+module.exports = {
+	//todo context - обычная строчка которая говорит где все исходники
+	context: path.resolve(__dirname, 'src'),
+	//todo mode
+	mode: 'development',
+	//todo входная точка проекта
+	entry: {
+		main: ['@babel/polyfill', './index.js'],
+		analytics: './analytics.js',
+	},
+	//todo куда выводить
+	output: {
+		//? название выводимого файла
+		// filename: 'bundle.js',
+		//? название выводимого файла - расширенный вар
+		// filename: '[name].[contenthash].js',
+		filename: filename('js'),
+		//? путь к файлу
+		path: path.resolve(__dirname, 'dist'),
+	},
+	//todo указывать - с каким расширениями будет работать- тогда при импорте расширения можно будет опустить
+	resolve: {
+		extensions: ['.js', '.json', '.png'],
+		//* указывает на корень приложения
+		alias: {
+			'@models': path.resolve(__dirname, 'src/models'),
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	//todo оптимизация - чтобы не грузить все библиотеки несколько раз
+	optimization: optimize(),
+	//todo devServer
+	devServer: {
+		contentBase: './dist',
+		hot: isDev ? true : false,
+		port: 2424,
+		liveReload: true,
+	},
+
+	//todo devTool - контролирует как source map комплируется
+	devtool: isDev ? 'source-map' : false,
+	//todo плагины
+	plugins: [
+		//* htmlWebpack Plugin
+		new HtmlWebpackPlugin({
+			//? путь до template
+			template: './index.html',
+			minify: {
+				collapseWhitespace: isProd,
+			},
+		}),
+		//* cleanWebpackPlugin - очищает папку dist
+		new CleanWebpackPlugin(),
+		//* copyWebpackPlugin - откуда и куда копировать
+		/* new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, './src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+    ]), */
+		new CopyWebpackPlugin({
+			patterns: [
+				//* если путь не указан, то по дефолту - в папку dist
+				{
+					from: './favicon.ico',
+					to: '',
+				},
+			],
+		}),
+		//todo для создания отдельного css файла
+		new MiniCssExtractPlugin({
+			filename: filename('css'),
+		}),
+
+		new BundleAnalyzerPlugin(),
+	],
+	//* лоадеры - возможность дополнения к webpack функционала, позволяющую ему работать с другими типами файлов (css, jpg, png, xml и т.д.)
+	module: {
+		rules: [
+			{
+				//* с использовани функции
+				test: /\.css$/,
+				use: cssLoaders(),
+			},
+			//todo для препроцессора scss
+			{
+				test: /\.s[ac]ss$/,
+				//* через функцию css loaders
+				use: cssLoaders('sass-loader'),
+			},
+			//todo для file loader - чтобы грузить картинки
+			{
+				test: /\.(png|jpg|svg|gif)$/,
+				use: ['file-loader'],
+			},
+			//todo для работы со шрифтами
+			{
+				test: /\.(ttf|woff|woff2|eot)$/,
+				use: ['file-loader'],
+			},
+
+			//todo babel js
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					//* в начале
+					// loader: 'babel-loader',
+					//* через функцию
+					loader: jsLoaders(),
+					//* через функцию
+					options: babelOptions(),
+				},
+			},
+		],
+	},
+};
